a516838da9ea30bb1a865e482f79a4bea9ae5ce0
Zeynettin Karabiber <86365816+zeynettin@users.noreply.github.com>
2022-06-15 00:49:32 +0300

    Diamond java program ++

---
+++ Java101/Loops/DiamondOfAsterikss.java
@@ -0,0 +1,31 @@
+package Java101.Loops;
+
+import java.util.*;
+
+public class DiamondOfAsterikss {
+    public static void main(String[] args) {
+        Scanner klavye = new Scanner(System.in);
+        System.out.print("Bir Sayı Giriniz : ");
+        int n = klavye.nextInt();
+
+        for (int i = 0; i <= n ; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        for ( int l = 1; l <= n; l++){
+            for (int j = 0; j < l; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k  <= 2*(n-l+1)-1 ; k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+
+}
